df0efe3024355f923cdd01e9cdf8965c54df2336
smangajamessithole@gmail.com <smangajamessithole@gmail.com>
2026-07-08 10:48:35 +0200

    Use CompressedStaticFilesStorage

--- myProject/settings/production.py
+++ myProject/settings/production.py
@@ -31,14 +31,18 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
+
 
 MIDDLEWARE.insert(
     1,
